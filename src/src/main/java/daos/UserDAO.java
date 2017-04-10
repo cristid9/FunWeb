e12146640defc86a67230b82b951f45aca360cc6
mariusolaru <mariusolaru1997@yahoo.com>
2017-04-10 14:12:48 +0300

    Added Stub in UserDAO

--- src/src/main/java/daos/UserDAO.java
+++ src/src/main/java/daos/UserDAO.java
@@ -11,18 +11,18 @@ import java.util.List;
 
 public class UserDAO {
 
-    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    private JdbcTemplate jdbcTemplate;
-    private SimpleJdbcInsert insert;
+    //private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+   // private JdbcTemplate jdbcTemplate;
+    //private SimpleJdbcInsert insert;
 
     // add crud methods here
 
 
     public User getUser(Long id){
-        String sql = "select name from User where id=" + id +";";
+        //String sql = "select name from User where id=" + id +";";
 
-        HashMap<String, Long> parameters  = new HashMap<String, Long>();
-        parameters.put("id" , id);
+        //HashMap<String, Long> parameters  = new HashMap<String, Long>();
+        //parameters.put("id" , id);
 
         //List<User> query = namedParameterJdbcTemplate.query(sql , parameters , userRowMapper);
 
