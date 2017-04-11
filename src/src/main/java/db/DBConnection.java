54079bb61e3cd9b772300f057fc57bfc95d186d0
cristid9 <cristi.dnla@gmail.com>
2017-04-11 13:51:11 +0300

    Completed DBConnection

--- src/src/main/java/db/DBConnection.java
+++ src/src/main/java/db/DBConnection.java
@@ -1,6 +1,32 @@
 package main.java.db;
 
 
-public class DBConnection {
+import oracle.jdbc.pool.OracleDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+class DBConnection {
+
+    String jdbcUrl = " jdbc:oracle:thin:@127.0.0.1:1521:XE";
+    String userid = "tw2017";
+    String password = "tw2017";
+    Connection conn;
+
+    public DBConnection() {
+        try {
+            Class.forName ("oracle.jdbc.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getDBConnection() throws SQLException {
+        OracleDataSource ds = new OracleDataSource();
+        ds.setURL(jdbcUrl);
+        conn = ds.getConnection(userid, password);
+
+        return conn;
+    }
 
 }
