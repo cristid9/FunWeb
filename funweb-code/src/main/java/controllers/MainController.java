e1df62c064bf70a578935eaf32c90a74b5d7db6e
cristid9 <cristi.dnla@gmail.com>
2017-04-12 18:31:20 +0300

    Added username check

--- funweb-code/src/main/java/controllers/MainController.java
+++ funweb-code/src/main/java/controllers/MainController.java
@@ -2,6 +2,9 @@ package controllers;
 
 import daos.UserDAO;
 import db.DBConnection;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,24 +14,40 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class MainController {
 
-    UserDAO dao = new UserDAO(new DBConnection());
+    private UserDAO dao = new UserDAO(new DBConnection());
 
     @RequestMapping(value = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
 
-        dao.createUser(null);
 
         return "register";
     }
 
-
     @ResponseBody
     @RequestMapping(value = "/checkUsernameAvailable", method = RequestMethod.POST)
     public String checkValidUsername(@RequestParam String username) {
+        JSONObject json = new JSONObject();
+        JSONArray jsonArray = new JSONArray();
 
+        String suggestion = dao.checkIfValidUsername(username);
+        if (suggestion != null) {
+            try {
+                json.put("status", "taken");
+                json.put("suggestion", suggestion);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
+            return json.toString();
+        } else {
+            try {
+                json.put("status", "ok");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
-        return "dummy";
+            return json.toString();
+        }
     }
 
     @ResponseBody
