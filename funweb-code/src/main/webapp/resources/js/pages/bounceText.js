6d921c443716c2bd7acbb2970660733eabca2977
BogdanB01 <bocabogdan2010@yahoo.com>
2017-04-28 23:41:19 +0300

    Added javascript directory to project structure

---
+++ funweb-code/src/main/webapp/resources/js/pages/bounceText.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.bounce').textillate({
+        loop: true,
+        in: {
+            effect: 'tada',
+            delayScale: 1,
+            delay: 150,
+            shuffle: true
+        },
+        out: {
+            effect: 'flipOutY',
+            reverse: true
+        }
+    });
+
+})
