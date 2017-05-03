5e43acd996129497506c2dfcdebad3ce108e1473
cristid9 <cristi.dnla@gmail.com>
2017-05-03 17:11:22 +0300

    Added method to send registartion notification mails

--- funweb-code/src/main/java/funWebMailer/FunWebMailer.java
+++ funweb-code/src/main/java/funWebMailer/FunWebMailer.java
@@ -1,7 +1,9 @@
 package funWebMailer;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 
 /**
  * Created by Bogdanel on 03.05.2017.
@@ -12,6 +14,34 @@ public class FunWebMailer {
 
     private void sendTextMail(List<String> recipients, String subject, String text){
         //stub method
+    }
+
+    /**
+     * @brief Sends the notification mail that every user should get after completing the
+     *        registration form.
+     * @param name The name int the real life of the user.
+     * @param email The email address of this use.
+     */
+    // TODO: Externalize hardcoded mail string to specified templates
+    // MYBE TODO: refactor this method to get an `user` as argument
+    public static void sendTextRegisterNotification(String name, String email) {
+        List<String> recipients = new ArrayList<String>();
+        recipients.add(email);
+
+        String mailContentUnformated = "Salut %s," +
+                "Am inregistrat o tentativa de logare de pe aceasta adresa de mail\n" +
+                " pentru a-ti activa contul da click pe acest link %s.\n" +
+                "Multumesc mult\n," +
+                "Echipa FunWeb";
+
+        // really really bad practice, but it will be allowed for the moment
+        String resetPasswordUrl = "localhost:8083/reset/" + UUID.randomUUID().toString();
+
+        String mailContent = String.format(mailContentUnformated, name, resetPasswordUrl);
+
+        String subject = "Confirm your registration attempt at FunWeb";
+
+        sendTextMail(recipients, subject, mailContent);
     }
 
 }
