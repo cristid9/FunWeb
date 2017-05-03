125763eaefe7735f430afb79f82d5483e497ee8e
BogdanB01 <bocabogdan2010@yahoo.com>
2017-05-03 17:31:43 +0300

    Added implementation to send mail

--- funweb-code/src/main/java/funWebMailer/FunWebMailer.java
+++ funweb-code/src/main/java/funWebMailer/FunWebMailer.java
@@ -1,5 +1,13 @@
 package funWebMailer;
 
+import com.sun.org.apache.xpath.internal.operations.Mult;
+import com.sun.xml.internal.ws.protocol.soap.MessageCreationException;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import java.util.List;
 import java.util.Properties;
 
@@ -8,10 +16,43 @@ import java.util.Properties;
  */
 public class FunWebMailer {
 
-    private static final String sender = "funwebcontact@gmail.com";
+    private static final String from = "funwebcontact@gmail.com";
+    private static final String password = "funweb2017";
 
-    private void sendTextMail(List<String> recipients, String subject, String text){
-        //stub method
+    private static void sendTextMail(List<String> recipients, String subject, String text){
+        Properties props = System.getProperties();
+        String host = "smtp.gmail.com";
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.user", from);
+        props.put("mail.smtp.password", password);
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(from));
+            InternetAddress[] toAddress = new InternetAddress[recipients.size()];
+
+            for(int i = 0; i < recipients.size(); i++){
+                toAddress[i] = new InternetAddress(recipients.get(i));
+            }
+
+            for( int i = 0; i < toAddress.length; i++) {
+                message.addRecipient(Message.RecipientType.TO, toAddress[i]);
+            }
+
+            message.setSubject(subject);
+            message.setText(text);
+            Transport transport = session.getTransport("smtp");
+            transport.connect(host, from, password);
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        } catch (MessagingException me) {
+            me.printStackTrace();
+        }
+
     }
-
 }
