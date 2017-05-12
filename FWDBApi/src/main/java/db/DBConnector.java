97f80692d4aae420cfd476c7c581715432597d0d
cristid9 <cristi.dnla@gmail.com>
2017-05-12 21:01:48 +0300

    Added initial files for the User REST service

---
+++ FWDBApi/src/main/java/db/DBConnector.java
@@ -0,0 +1,57 @@
+package db;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import oracle.jdbc.pool.OracleDataSource;
+import org.springframework.beans.factory.annotation.Value;
+
+public class DBConnector {
+
+    public class DBConnection {
+        @Value("current.app.url")
+        String dbUrl;
+
+        @Value("current.app.sid")
+        String dbSid;
+
+        @Value("current.app.port")
+        String dbPort;
+
+        @Value("current.app.user")
+        String dbUser;
+
+        @Value("current.app.password")
+        String dbPassword;
+
+        String jdbcUrl = String.format("jdbc:oracle:thin:@%s:%s:%s", dbUrl, dbPort, dbSid);
+        Connection conn;
+
+        public DBConnection() {
+
+            try {
+                Class.forName("oracle.jdbc.OracleDriver");
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+            OracleDataSource ds = null;
+
+            try {
+                ds = new OracleDataSource();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            ds.setURL(jdbcUrl);
+
+            try {
+                conn = ds.getConnection(dbUser, dbPassword);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public Connection getDBConnection() throws SQLException {
+            return conn;
+        }
+    }
+}
