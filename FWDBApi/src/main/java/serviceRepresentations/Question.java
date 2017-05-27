6073d89a1aa0fab3dbda02dbd6592c41fc6b6dca
cristid9 <cristi.dnla@gmail.com>
2017-05-27 22:56:39 +0300

    Added question POJO

---
+++ FWDBApi/src/main/java/serviceRepresentations/Question.java
@@ -0,0 +1,19 @@
+package serviceRepresentations;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * POJO for a game question.
+ */
+@AllArgsConstructor
+@Getter
+@Setter
+public class Question {
+    public Long id;
+    public String enunciation;
+    public Long reward;
+    public Long characterId;
+    public Long chapterId;
+}
