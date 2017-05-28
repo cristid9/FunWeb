20e87faeee4210afdddc55bdb05173c2b794fff0
cristid9 <cristi.dnla@gmail.com>
2017-05-28 18:30:32 +0300

    Created skeleton for createQuestion endpoint

--- FWDBApi/src/main/java/serviceComponents/QuestionController.java
+++ FWDBApi/src/main/java/serviceComponents/QuestionController.java
@@ -2,8 +2,14 @@ package serviceComponents;
 
 import db.DBConnector;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import serviceRepresentations.Question;
 import serviceResources.QuestionDAO;
 
 @RestController
@@ -14,5 +20,28 @@ public class QuestionController {
     DBConnector dbConnector;
     QuestionDAO questionDAO;
 
+    /**
+     * Endpoint for creating a new question.
+     * @param question A JSON representing the new question to be inserted.
+     * @return BAD_REQUEST if the question is invalid, OK otherwise.
+     */
+    @RequestMapping(
+            value = "/",
+            method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<Long> createQuestion(@RequestBody Question question) {
+        questionDAO = new QuestionDAO(dbConnector);
+        Long id = questionDAO.createQuestion(question);
+
+        // TODO: fields check
+        // TODO: security
+
+        if (id == -1) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(id, HttpStatus.OK);
+    }
 
 }
