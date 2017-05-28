a0dcdbf9bb754e3f64909b49b9f4bac2667a0ecd
cristid9 <cristi.dnla@gmail.com>
2017-05-28 18:13:11 +0300

    Added skeleton for QuestionController

--- FWDBApi/src/main/java/serviceComponents/QuestionController.java
+++ FWDBApi/src/main/java/serviceComponents/QuestionController.java
@@ -1,7 +1,18 @@
 package serviceComponents;
 
-/**
- * Created by cristi on 27.05.2017.
- */
+import db.DBConnector;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import serviceResources.QuestionDAO;
+
+@RestController
+@RequestMapping("/v1/question")
 public class QuestionController {
+
+    @Autowired
+    DBConnector dbConnector;
+    QuestionDAO questionDAO;
+
+
 }
