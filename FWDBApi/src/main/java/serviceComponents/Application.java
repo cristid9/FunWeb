2aa6ffd6a545a839e4a77ffd8b880e1ba095b697
cristid9 <cristi.dnla@gmail.com>
2017-06-02 19:14:45 +0300

    Added JPA exclusion in main class

--- FWDBApi/src/main/java/serviceComponents/Application.java
+++ FWDBApi/src/main/java/serviceComponents/Application.java
@@ -3,11 +3,18 @@ package serviceComponents;
 import db.DBConnector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+        ErrorMvcAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
 @ComponentScan
 public class Application {
 
