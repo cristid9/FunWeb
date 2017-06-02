f9020fafd91ff0dcf187b1579340dfb4e5bc82a5
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-02 15:46:42 +0300

    Added script for creating tables

---
+++ FWDBApi/src/main/resources/dbScripts/create_tables.sql
@@ -0,0 +1,149 @@
+----- tabele ------
+
+drop table Chapters;
+/
+create table Chapters (
+  id NUMBER (10, 0),
+  chapter_name varchar2(200)
+);  
+
+/
+drop table Hints;
+/
+create table Hints(
+  id NUMBER (10, 0),
+  question_id NUMBER(10, 0),
+  text varchar2(200)
+);
+/
+drop table Character;
+/
+create table Characters(
+  id NUMBER (10, 0),
+  name varchar2(200),
+  picture_path varchar2(200),
+  question_number NUMBER (10, 0)
+);
+/
+drop table logindatacustom;
+/
+create table logindatacustom(
+  id NUMBER(10, 0),
+  user_id NUMBER(10, 0),
+  password varchar2(500)
+);
+/
+drop table logindatasocial;
+/
+create table logindatasocial(
+  id number(10, 0),
+  auto_hash varchar2(500),
+  user_id number(10, 0)
+);
+/
+drop table Options;
+/
+create table Options(
+  id number(10, 0),
+  enunciation varchar2(1000),
+  correctness number(10, 0),
+  questions_question_id number(10, 0)
+);
+/
+drop table Questions;
+/
+create table Questions(
+  question_id number(10, 0),
+  enunciation varchar2(500),
+  reward number(10, 0),
+  chapter_id number(10, 0),
+  characters_id number(10, 0)
+);
+/
+drop table USERS;
+/
+create table users(
+  id number(10, 0),
+  name varchar2(200),
+  user_role varchar2(200),
+  email varchar2(200),
+  login_type varchar2(200),
+  hints_left number(10, 0),
+  gold_left number(10, 0),
+  avatar_path varchar2(200),
+  user_level number(10, 0)
+);
+
+/
+create sequence chapters_id_seq;
+/
+create sequence hints_id_seq;
+/
+create sequence characters_id_seq;
+/
+create sequence logindatacustom_id_seq;
+/
+create sequence logindatasocial_id_seq;
+/
+create sequence options_id_seq;
+/
+create sequence questions_id_seq;
+/
+create sequence user_id_seq;
+
+/
+create or replace trigger trg_chapters_id
+  before insert on chapters
+  for each row
+begin
+  select chapters_id_seq.nextval into :new.id from dual;
+end;
+/
+create or replace trigger trg_hints_id
+  before insert on hints
+  for each row
+begin
+  select hints_id_seq.nextval into :new.id from dual;
+end;
+/
+create or replace trigger trg_characters_id
+  before insert on characters
+  for each row
+begin
+  select characters_id_seq.nextval into :new.id from dual;
+end;
+/
+create or replace trigger trg_logindatacustom_id
+  before insert on logindatacustom
+  for each row
+begin
+  select logindatacustom_id_seq.nextval into :new.id from dual;
+end;
+/
+create or replace trigger trg_logindatasocial_id
+  before insert on logindatasocial
+  for each row
+begin
+  select logindatasocial_id_seq.nextval into :new.id from dual;
+end;
+/
+create or replace trigger trg_options_id
+  before insert on options
+  for each row
+begin
+  select options_id_seq.nextval into :new.id from dual;
+end;
+/
+create or replace trigger trg_questions_id
+  before insert on questions
+  for each row
+begin
+  select questions_id_seq.nextval into :new.question_id from dual;
+end;
+/
+create or replace trigger trg_users_id
+  before insert on users
+  for each row
+begin
+  select user_id_seq.nextval into :new.id from dual;
+end;
