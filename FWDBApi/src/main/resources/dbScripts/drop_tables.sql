7f49006deaf5b5c5c6fdb92a7364fa00fa3b82a8
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-02 16:51:51 +0300

    Added script for droping tables

---
+++ FWDBApi/src/main/resources/dbScripts/drop_tables.sql
@@ -0,0 +1,48 @@
+drop table Chapters;
+/
+drop table Hints;
+/
+drop table Characters;
+/
+drop table LoginDataCustom;
+/
+drop table LoginDataSocial;
+/
+drop table Options;
+/
+drop table Questions;
+/
+drop table Users;
+/
+drop sequence chapters_id_sq;
+/
+drop sequence hints_id_seq;
+/
+drop sequence characters_id_seq;
+/
+drop sequence logindatacustom_id_seq;
+/
+drop sequence logindatasocial_id_seq;
+/
+drop sequence options_id_seq;
+/
+drop sequence questions_id_seq;
+/
+drop sequence user_id_seq;
+/
+drop trigger trg_chapters_id;
+/
+drop trigger trg_hints_id;
+/
+drop trigger trg_chapters_id;
+/
+drop trigger trg_logindatacustom_id;
+/
+drop trigger trg_logindatasocial_id;
+/
+drop trigger trg_options_id;
+/
+drop trigger trg_questions_id;
+/
+drop trigger trg_users_id;
+/
