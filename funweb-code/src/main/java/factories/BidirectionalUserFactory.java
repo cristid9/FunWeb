ceaa1f106877935a87e274e9cebebce3bd0abc85
cristid9 <cristi.dnla@gmail.com>
2017-06-03 15:37:39 +0300

    Added Bidirectional factory template

---
+++ funweb-code/src/main/java/factories/BidirectionalUserFactory.java
@@ -0,0 +1,16 @@
+package factories;
+
+import pojos.User;
+
+public class BidirectionalUserFactory {
+
+    // overload may be added
+    public static User newInstance() {
+        return null;
+    }
+
+    public static Boolean persist(User user) {
+        return null;
+    }
+
+}
