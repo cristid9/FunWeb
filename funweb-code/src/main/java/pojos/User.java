7e5ac335cdd2c7246916036c2912bfbd7d587e17
mariusolaru <mariusolaru1997@yahoo.com>
2017-06-03 13:51:30 +0300

    Added User pojo

---
+++ funweb-code/src/main/java/pojos/User.java
@@ -0,0 +1,26 @@
+package pojos;
+
+/**
+ * Created by Marius on 6/3/2017.
+ */
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * POJO for a game's user.
+ */
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    public Long id;
+    public String name;
+    public String userRole;
+    public String email;
+    public String loginType;
+    public Integer level;
+    public Integer hintsLeft;
+    public Integer goldLeft;
+    public String avatarPath;
+}
