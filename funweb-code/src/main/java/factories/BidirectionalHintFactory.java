34d38fdba14220411296abceda751ad83282ceda
cristid9 <cristi.dnla@gmail.com>
2017-06-03 21:24:18 +0300

    Implemented persist method in hints factory

--- funweb-code/src/main/java/factories/BidirectionalHintFactory.java
+++ funweb-code/src/main/java/factories/BidirectionalHintFactory.java
@@ -4,6 +4,8 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.mashape.unirest.request.HttpRequestWithBody;
+import com.mashape.unirest.request.body.MultipartBody;
 import org.json.JSONException;
 import org.json.JSONObject;
 import pojos.Hint;
@@ -24,9 +26,9 @@ public class BidirectionalHintFactory {
     private static final String FIELD_QUESTION_ID = "questionId";
 
     /**
-     * Gets the attributes of an user from de db service.
-     * @param userName Username that identifies this user.
-     * @return A new user instance.
+     * Gets the attributes of an hint from de db service.
+     * @param id identifies this hint.
+     * @return A new hint instance.
      * @throws UnirestException
      */
     // TODO: Lacks security
@@ -57,13 +59,41 @@ public class BidirectionalHintFactory {
 
     public static void main(String[] args) {
         try {
-            newInstance(1l);
+            Hint hint = new Hint(1l, 1l, "am valuta");
+            BidirectionalHintFactory.persist(hint);
+
         } catch (UnirestException e) {
             e.printStackTrace();
         }
     }
 
-    public static Boolean persist(Hint hint) {
-        return null;
+    public static Boolean persist(Hint hint) throws UnirestException {
+        String url = String.format("http://%s:%s/%s/hint/",
+                REQUEST_ADDRESS, REQUEST_PORT, API_VERSION);
+
+
+        JSONObject jsonObject = null;
+        try {
+            jsonObject = new JSONObject();
+
+            jsonObject.put(FIELD_ID, hint.getId());
+            jsonObject.put(FIELD_QUESTION_ID, hint.getQuestionId());
+            jsonObject.put(FIELD_TEXT, hint.getText());
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return Boolean.FALSE;
+        }
+
+
+        HttpResponse<JsonNode> httpResponse = Unirest.post(url)
+                .header("Content-Type", "application/json")
+                .body(jsonObject.toString())
+                .asJson();
+
+        if (httpResponse.getStatus() == 201) {
+            return Boolean.TRUE;
+        }
+
+        return Boolean.FALSE;
     }
 }
