da367a9b2e3aaab4f0101573a2bbc117b666ba9b
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-06 21:54:41 +0300

    Added LoggedUser class

---
+++ FWDBApi/src/main/java/serviceRepresentations/LoggedUser.java
@@ -0,0 +1,15 @@
+package serviceRepresentations;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class LoggedUser {
+    private Long id;
+    private String userName;
+}
