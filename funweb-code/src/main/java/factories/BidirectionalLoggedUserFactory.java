7eba0f0e82494b3dc90a85554ab7c44762c2b264
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-07 02:28:41 +0300

    Added getAll method in LoggedUsers Factory

--- funweb-code/src/main/java/factories/BidirectionalLoggedUserFactory.java
+++ funweb-code/src/main/java/factories/BidirectionalLoggedUserFactory.java
@@ -4,9 +4,14 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import pojos.LoggedUser;
+import sun.rmi.runtime.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static factories.FactoryConfig.API_VERSION;
 import static factories.FactoryConfig.REQUEST_ADDRESS;
@@ -51,10 +56,13 @@ public class BidirectionalLoggedUserFactory {
     }
 
     public static void main(String[] args) {
-        LoggedUser user = new LoggedUser(1L, "Cristi");
 
         try {
-            remove(user);
+            List<LoggedUser> users = new ArrayList<LoggedUser>();
+            users = getAll();
+
+            for(LoggedUser l : users)
+                System.out.println(l.getUserName());
         } catch (UnirestException e) {
             e.printStackTrace();
         }
@@ -68,6 +76,27 @@ public class BidirectionalLoggedUserFactory {
         if(response.getStatus() == 200)
             return Boolean.TRUE;
         return Boolean.FALSE;
+    }
+
+    public static List<LoggedUser> getAll() throws UnirestException {
+        String url = String.format("http://%s:%s/%s/loggeduser/all",
+                REQUEST_ADDRESS, REQUEST_PORT, API_VERSION);
+        HttpResponse<JsonNode> response = Unirest.get(url).asJson();
+        List<LoggedUser> loggedUsers = new ArrayList<LoggedUser>();
+        try {
+            JSONArray jsons = new JSONArray(response.getBody().getArray().toString());
+            for(int i = 0; i < jsons.length(); i++){
+                JSONObject jsonObject = jsons.getJSONObject(i);
+                LoggedUser loggedUser = new LoggedUser();
+                loggedUser.setId(jsonObject.getLong(FIELD_ID));
+                loggedUser.setUserName(jsonObject.getString(FIELD_USER_NAME));
+
+                loggedUsers.add(loggedUser);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return loggedUsers;
     }
 
     public static Boolean persist( LoggedUser loggedUser) throws UnirestException {
