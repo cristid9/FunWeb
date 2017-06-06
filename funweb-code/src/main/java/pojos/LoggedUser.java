9bca220240874aee20b7a5991a25b0bb41a4bdfc
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-06 23:35:41 +0300

    Added LoggedUser class to pojos package

---
+++ funweb-code/src/main/java/pojos/LoggedUser.java
@@ -0,0 +1,15 @@
+package pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class LoggedUser {
+    private Long id;
+    private String userName;
+}
