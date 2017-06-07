124dc395abca1e30ee3a1a049463ff6d3a947105
cristid9 <cristi.dnla@gmail.com>
2017-06-07 16:56:06 +0300

    Added PendingPasswordReset

---
+++ funweb-code/src/main/java/factories/BidirectionalPendingPasswordResetFactory.java
@@ -0,0 +1,7 @@
+package factories;
+
+/**
+ * Created by cristi on 07.06.2017.
+ */
+public class BidirectionalPendingPasswordResetFactory {
+}
---
+++ funweb-code/src/main/java/pojos/PendingPasswordReset.java
@@ -0,0 +1,16 @@
+package pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class PendingPasswordReset {
+    Long id;
+    String token;
+    String username;
+}
