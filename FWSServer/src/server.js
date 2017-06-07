4ebf9942d258e8aae2dd7251a0f5b5f355a92ede
cristid9 <cristi.dnla@gmail.com>
2017-06-07 23:23:20 +0300

    Modified websockets server, integrated George's work

--- FWSServer/src/server.js
+++ FWSServer/src/server.js
@@ -1,29 +1,48 @@
-// Require HTTP module (to start server) and Socket.IO
+var express =require('express'),
+ http = require('http');
+var app= express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
-var http = require('http'), io = require('socket.io');
+users = [];
+connections = [];
 
-// Start the server at port 8080
-var server = http.createServer(function(req, res){
+// Not that modular yet
 
-    // Send HTML headers and message
-    res.writeHead(200,{ 'Content-Type': 'text/html' });
-    res.end('<h1>Hello Socket Lover!</h1>');
+server.listen(process.env.Port || 3000);
+console.log('Server running...')
+
+app.get('/', function(req, res) {
+ res.sendFile(__dirname + '/index.html');
 });
-server.listen(6666);
 
-// Create a Socket.IO instance, passing it our server
-var socket = io.listen(server);
+io.sockets.on('connection', function(socket){
+ connections.push(socket);
+ console.log('Connected: %s sockets connected', connections.length);
 
-// Add a connect listener
-socket.on('connection', function(client){
-
-    // Success!  Now listen to messages to be received
-    client.on('message',function(event){
-        console.log('Received message from client!',event);
-    });
-    client.on('disconnect',function(){
-        clearInterval(interval);
-        console.log('Server has disconnected');
-    });
-
+ socket.on('disconnect',function(data){
+ users.splice(users.indexOf(socket.username),1);
+ updateUsernames();
+ connections.splice(connections.indexOf(socket),1);
+ console.log('Disconnected: %s sockets conected', connections.length);
 });
+
+ //send message
+socket.on('send message', function(data){
+  console.log(data)
+  io.sockets.emit('new message', {msg: data, user: socket.username});
+});
+
+ //new User
+
+socket.on('new user', function(data,callback){
+  callback(true);
+  socket.username = data;
+  users.push(socket.username);
+  updateUsernames();
+});
+
+ function updateUsernames(){
+  io.sockets.emit('get users', users)
+ }
+});﻿
