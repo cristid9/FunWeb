f984cc1f15f0d43f3a48100560d3a00df7edd453
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-08 20:55:09 +0300

    Fixed conflicts

---
+++ funweb-code/target/funweb-1.0-SNAPSHOT/resources/js/game_tutorial/gameMain.js
@@ -0,0 +1,7 @@
+var game = new Phaser.Game(document.body.offsetWidth, document.body.offsetHeight, Phaser.AUTO, 'funweb-game');
+
+game.state.add('loadingState', loadingState);
+game.state.add('mainState', mainState);
+game.state.add('questionsState', questionsState);
+
+game.state.start('loadingState');
---
+++ funweb-code/target/funweb-1.0-SNAPSHOT/resources/js/game_tutorial/loadingState.js
@@ -0,0 +1,44 @@
+var ready = false;
+
+var loadingState = {
+    preload: function () {
+        this.game.load.image('mainPlayer', '/resources/assets/mainPlayer.png');
+        this.game.load.image('backendQuestioner', '/resources/assets/backendQuestioner.png');
+        this.game.load.image('frontendQuestioner', '/resources/assets/frontendQuestioner.png');
+        this.game.load.image('sqlQuestioner', '/resources/assets/sqlQuestioner.png');
+        this.game.load.spritesheet('trotinel', '/resources/assets/playerSprite.png', 80, 110, 24);
+        this.game.load.image('noSqlQuestioner', '/resources/assets/noSqlQuestioner.png');
+        this.game.load.image('acceptButton', '/resources/assets/accept.png');
+        this.game.load.image('dismissButton', '/resources/assets/dismiss.png');
+        this.game.load.image('questionerDialogPanel', '/resources/assets/questionerDialogPanel.png');
+        this.game.load.tilemap('map', '/resources/assets/map.json', null, Phaser.Tilemap.TILED_JSON);
+        this.game.load.image('tiles', '/resources/assets/terrain.png');
+        this.game.load.image('tiles1', '/resources/assets/trees.png');
+
+
+        ready = true;
+    },
+
+    create: function () {
+        this.game.stage.backgroundColor = '#aaa';
+
+        this.guy = this.game.add.sprite(570, this.game.world.centerY, 'trotinel');
+        this.guy.scale.setTo(1.5, 1.5);
+
+        this.waitBubble = this.game.add.sprite(700, 200, 'questionerDialogPanel');
+        this.waitBubble.scale.setTo(1.5, 1.5);
+        this.waitText = this.game.add.text(700, 200, '\n   Loading \n    Game', {
+            font: "25px Arial"
+        });
+
+        this.guy.animations.add('idle', [0, 23] , 3, true);
+        this.guy.animations.play('idle');
+
+    },
+
+    update: function() {
+        if(ready === true) {
+            this.state.start('mainState');
+        }
+    }
+};
