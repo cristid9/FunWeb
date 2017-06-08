f62dc6da44e4a0c5c8c04f144d435a3be40cab23
BogdanB01 <bocabogdan2010@yahoo.com>
2017-06-09 02:07:28 +0300

    Deleted target folder from project structure

--- funweb-code/target/funweb-1.0-SNAPSHOT/resources/js/game_tutorial/gameMain.js
+++
@@ -1,7 +0,0 @@
-var game = new Phaser.Game(document.body.offsetWidth, document.body.offsetHeight, Phaser.AUTO, 'funweb-game');
-
-game.state.add('loadingState', loadingState);
-game.state.add('mainState', mainState);
-game.state.add('questionsState', questionsState);
-
-game.state.start('loadingState');
